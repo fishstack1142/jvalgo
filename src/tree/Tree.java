70ee46d822a030d3e409cf16c0a45d50ea39a2f9
fishstack1142 <fishstack1142@gmail.com>
2020-03-01 16:36:40 +0700

    Tree structure

---
+++ src/tree/Tree.java
@@ -0,0 +1,72 @@
+package tree;
+
+public class Tree {
+
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+        tree.insert(7);
+        tree.insert(4);
+        tree.insert(9);
+        tree.insert(1);
+        tree.insert(6);
+        tree.insert(8);
+        tree.insert(10);
+        System.out.println(tree.find(10));
+    }
+
+
+    private class Node {
+        private int value;
+        private Node leftChild;
+        private Node rightChild;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Node=" + value;
+        }
+    }
+
+    private Node root;
+
+    public void insert(int value) {
+        Node node = new Node(value);
+
+        if (root == null) {
+            root = node;
+            return;
+        }
+        Node current = root;
+        while (true) {
+            if (value > current.value) {
+                if (current.leftChild == null) {
+                    current.leftChild = node;
+                    break;
+                }
+                current = current.leftChild;
+            } else {
+                if (current.rightChild == null) {
+                    current.rightChild = node;
+                    break;
+                }
+                current = current.rightChild;
+            }
+        }
+    }
+
+    public boolean find(int value) {
+        Node current = root;
+        while (current != null) {
+            if (value < current.value)
+                current = current.leftChild;
+            else if (value > current.value)
+                current = current.rightChild;
+            else
+                return true;
+        }
+        return false;
+    }
+}
