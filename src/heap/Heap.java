169cafbc76fc0a18cc65b079f943de57cc2a42df
fishstack1142 <fishstack1142@gmail.com>
2020-03-11 02:24:43 +0700

    refactor code and add more method

--- src/heap/Heap.java
+++ src/heap/Heap.java
@@ -9,8 +9,9 @@ public class Heap {
         heap.insert(10);
         heap.insert(5);
         heap.insert(17);
-//        heap.insert(4);
-//        heap.insert(22);
+        heap.insert(4);
+        heap.insert(22);
+        heap.remove();
 
         System.out.println(Arrays.toString(heap.items));
     }
@@ -19,35 +20,106 @@ public class Heap {
     public int[] items = new int[10];
     private int size;
 
-     public void insert(int value) {
-         if (isFull())
-             throw new IllegalStateException();
+    public void insert(int value) {
+        if (isFull())
+            throw new IllegalStateException();
 
-         items[size++] = value;
+        items[size++] = value;
+        bubbleUp();
+    }
 
-         bubbleUp();
-     }
+    public int remove() {
+        if (isEmpty())
+            throw new IllegalStateException();
 
-     public boolean isFull() {
-         return size == items.length;
-     }
+        int root = items[0];
+        items[0] = items[--size];
 
-     private void bubbleUp() {
-         //if newItem > parent
-         int index = size - 1;
-         while (index > 0 && items[index] > items[parent(index)]) {
-             swap(index, parent(index));
-             index = parent(index);
-         }
-     }
+        bubbleDown();
+        return root;
+    }
 
-     private int parent(int index) {
-         return (index - 1) / 2;
-     }
+    private void bubbleDown() {
+        //item (root) < children
+        int index = 0;
+        while (index <= size && !isValidParent(index)) {
+            int largerChildIndex = largerChildIndex(index);
+            swap(index, largerChildIndex);
+            index = largerChildIndex;
+        }
+    }
 
-     private void swap(int first, int second) {
-         int temp = items[first];
-         items[first] = items[second];
-         items[second] = temp;
-     }
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private int largerChildIndex(int index) {
+        if (!hasLeftChild(index))
+            return index;
+
+        if (!hasRightChild(index))
+            return leftChildIndex(index);
+
+        return (leftChild(index) > rightChild(index)) ? leftChildIndex(index) : rightChildIndex(index);
+    }
+
+    private boolean hasLeftChild(int index) {
+        return leftChildIndex(index) <= size;
+    }
+
+    private boolean hasRightChild(int index) {
+        return rightChildIndex(index) <= size;
+    }
+
+    private boolean isValidParent(int index) {
+        if (!hasLeftChild(index))
+            return true;
+
+        boolean isValid = items[index] >= leftChild(index);
+
+        if (hasRightChild(index))
+            isValid &= items[index] >= rightChild(index);
+
+        return isValid;
+    }
+
+    private int rightChild(int index) {
+        return items[rightChildIndex(index)];
+    }
+
+    private int leftChild(int index) {
+        return items[leftChildIndex(index)];
+    }
+
+    private int leftChildIndex(int index) {
+        return index * 2 + 1;
+    }
+
+
+    private int rightChildIndex(int index) {
+        return index * 2 + 2;
+    }
+
+    public boolean isFull() {
+        return size == items.length;
+    }
+
+    private void bubbleUp() {
+        //if newItem > parent
+        int index = size - 1;
+        while (index > 0 && items[index] > items[parent(index)]) {
+            swap(index, parent(index));
+            index = parent(index);
+        }
+    }
+
+    private int parent(int index) {
+        return (index - 1) / 2;
+    }
+
+    private void swap(int first, int second) {
+        int temp = items[first];
+        items[first] = items[second];
+        items[second] = temp;
+    }
 }
