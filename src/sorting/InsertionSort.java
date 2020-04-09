50078cfb173093ed6d3935155be10fde0c2f0acd
fishstack1142 <fishstack1142@gmail.com>
2020-04-09 22:04:51 +0700

    insertion sort and selection sort

---
+++ src/sorting/InsertionSort.java
@@ -0,0 +1,26 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        int[] numbers = {7,3,1,5,2};
+        InsertionSort sorter = new InsertionSort();
+        sorter.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
+    }
+
+    public void sort(int[] array) {
+
+        for (int i =1; i < array.length; i++) {
+            int current = array[i];
+            int j = i -1;
+            while (j >= 0 && array[j] > current){
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = current;
+        }
+    }
+}
