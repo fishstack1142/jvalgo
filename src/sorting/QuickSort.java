ffa0e80d05abb5c476e8839a22ebff8de436e901
fishstack1142 <fishstack1142@gmail.com>
2020-04-11 23:22:34 +0700

    mergesort

---
+++ src/sorting/QuickSort.java
@@ -0,0 +1,40 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class QuickSort {
+
+    public static void main(String[] args) {
+        int[] numbers = {7,3,1,5,2};
+        QuickSort sorter = new QuickSort();
+        sorter.sort(numbers, 0, 4);
+        System.out.println(Arrays.toString(numbers));
+    }
+
+    public void sort(int[] array, int start, int end) {
+        if (start >= end)
+            return;
+
+        int boundary = partition(array, start, end);
+
+        sort(array, start, boundary - 1);
+        sort(array, boundary + 1, end);
+    }
+
+    private int partition(int[] array, int start, int end) {
+        int pivot = array[end];
+        int boundary = start -1;
+        for (int i = start; i <= end; i++)
+            if (array[i] <= pivot)
+//                boundary++;
+                swap(array, i, ++boundary);
+
+        return boundary;
+    }
+
+    private void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+}
